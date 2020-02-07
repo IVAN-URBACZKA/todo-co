493a441c4c3f6a6e24eeef4b292f4a74c298118e
URBACZKA IVAN <46192105+IVAN-URBACZKA@users.noreply.github.com>
2020-02-07 09:48:17 +0100

    add create roles form

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,14 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
 use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserController extends AbstractController
 {
     /**
-     * @Route("/user", name="user")
+     * @Route("/user", name="user_list")
      */
     public function index(UserRepository $repo)
     {
@@ -19,4 +23,32 @@ class UserController extends AbstractController
             'users' => $users
         ]);
     }
+
+    /**
+     * @Route("/users/create", name="user_create")
+     */
+    public function createAction(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $password = $passwordEncoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
+
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash('success', "L'utilisateur a bien été ajouté.");
+
+            return $this->redirectToRoute('user_list');
+        }
+
+        return $this->render('user/create.html.twig', ['form' => $form->createView()]);
+    }
+
+
 }
